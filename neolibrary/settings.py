866273291106c817fe8b757ec9df129d106c0898
Paolo Romolini <paoloromolini@gmail.com>
2018-11-21 21:24:05 +0100

    Update requirements

--- neolibrary/settings.py
+++ neolibrary/settings.py
@@ -90,7 +90,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'pt'
+LANGUAGE_CODE = 'en'
 
 TIME_ZONE = 'Europe/Rome'
 
