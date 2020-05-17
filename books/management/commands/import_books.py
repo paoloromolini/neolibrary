3427ad61c8f9a08bbc2540619773ae9c3415e6d5
Paolo Romolini <paoloromolini@gmail.com>
2020-05-17 21:37:15 +0200

    Change import script for porto biblio

--- books/management/commands/import_books.py
+++ books/management/commands/import_books.py
@@ -18,6 +18,11 @@ class Command(BaseCommand):
         )
 
         parser.add_argument(
+            '--genre',
+            help='Genre', required=False
+        )
+
+        parser.add_argument(
             '--json',
         help='json file path', required=False
         )
@@ -28,32 +33,41 @@ class Command(BaseCommand):
 
     def handle(self, *args, **options):
         file_path = options['csv']
+        genre_arg = options['genre']
         if file_path:
             with open(file_path, 'rt') as csvfile:
                 spamreader = csv.reader(csvfile, quotechar='"', delimiter=',',
                     quoting=csv.QUOTE_ALL, skipinitialspace=True)
                 for row in spamreader:
-                    title, author, editor, genre, language, year = row
-                    if title == 'Título':
-                        continue
+                    try:
+                        title, author, editor, genre, language, year = row
+                        if title == 'Título':
+                            continue
+                        if genre_arg:
+                            genre = genre_arg
+                        genre = genre.strip()
+                        g, __ = Genre.objects.get_or_create(
+                            name=genre)
+                        a, __ = Author.objects.get_or_create(
+                            name=author
+                        )
+                        p = Publisher.objects.filter(
+                            name=editor
+                        )
+                        if p:
+                            p = p[0]
+                        else:
+                            p = Publisher.objects.create(
+                                name=editor)
+                        b, __ = Book.objects.get_or_create(
+                            title=title,
+                            year_edition=int(year) if str.isdigit(year) else None,
+                            genre=g,
+                            publisher=p,
+                            column=0,
+                            number=0,
 
-                    g, __ = Genre.objects.get_or_create(
-                        name=genre,
-                        code=slugify(genre)
-                    )
-                    a, __ = Author.objects.get_or_create(
-                        name=author
-                    )
-                    p, __ = Publisher.objects.get_or_create(
-                        name=editor
-                    )
-                    b, __ = Book.objects.get_or_create(
-                        title=title,
-                        year_edition=int(year) if str.isdigit(year) else None,
-                        genre=g,
-                        publisher=p,
-                        column=0,
-                        number=0,
-
-                    )
-                    b.author.add(a)
+                        )
+                        b.author.add(a)
+                    except Exception as e:
+                        print('ERRORE ({}): {}'.format(e ,title))
