9717987b2b0d925cabe3eb4cf30b550dca7515f8
Paolo Romolini <paolo.romolini@enterprisedb.com>
2022-11-23 20:31:33 +0100

    Add Django Admin Restrict

--- neolibrary/settings.py
+++ neolibrary/settings.py
@@ -31,6 +31,7 @@ INSTALLED_APPS = [
     "django.contrib.staticfiles",
     "django.contrib.sites",
     "books",
+    "adminrestrict",
 ]
 
 MIDDLEWARE = [
@@ -41,6 +42,7 @@ MIDDLEWARE = [
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
     "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "adminrestrict.middleware.AdminPagesRestrictMiddleware",
 ]
 
 ROOT_URLCONF = "neolibrary.urls"
